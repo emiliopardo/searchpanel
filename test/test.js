46f67b6ea8e40c7088f9d200be26f3c33b942b00
emiliopardo <e.pardo.perez@gmail.com>
2022-02-25 08:35:07 +0100

    se actualiza configuración test.js

--- test/test.js
+++ test/test.js
@@ -4,224 +4,228 @@ const map = M.map({
   container: 'mapjs',
 });
 
-map.addControls(['Scale', 'ScaleLine','Mouse', 'panzoom','overviewmap','location','rotate']);
+map.addControls(['Scale', 'ScaleLine', 'Mouse', 'panzoom', 'overviewmap', 'location', 'rotate']);
 
 
 
 const configFuentesyManantiales = {
-  options:{
+  options: {
     position: 'BR'
   },
   config: {
-  title: 'Fuentes y Manantiales de Andalucía',
-  geosearchUrl: 'https://geobusquedas-sigc.juntadeandalucia.es/geobusquedas/fuentesymanantiales/search_html?',
-  maxResults: 100,
-  fields: [
-    {
-      field: 'nombre',
-      alias: 'Nombre',
-      label: 'Escribe el nombre',
-    },
-    {
-      field: 'municipio_fuente',
-      alias: 'Municipio',
-      label: 'Escribe el nombre del Municipio',
-    },
-    {
-      field: 'provincia_fuente',
-      alias: 'Provincia',
-      label: 'Escribe el nombre de la Provincia',
-    }
-  ],
-  infoFields: [
-    {
-      field: 'nombre',
-      alias: 'Nombre '
-    },
-    {
-      field: 'imagen',
-      alias: 'imagen'
-    },
-    {
-      field: 'tipo',
-      alias: 'Tipo'
-    },
-    {
-      field: 'enlace',
-      alias: 'Ficha'
-    },
-    {
-      field: 'municipio_fuente',
-      alias: 'Municipio'
-    },
-    {
-      field: 'provincia_fuente',
-      alias: 'Provincia'
-    }
-  ]
-}
+    title: 'Fuentes y Manantiales de Andalucía',
+    geosearchUrl: 'https://geobusquedas-sigc.juntadeandalucia.es/geobusquedas/fuentesymanantiales/search_html?',
+    maxResults: 100,
+    fields: [
+      {
+        field: 'nombre',
+        alias: 'Nombre',
+        label: 'Escribe el nombre',
+      },
+      {
+        field: 'municipio_fuente',
+        alias: 'Municipio',
+        label: 'Escribe el nombre del Municipio',
+      },
+      {
+        field: 'provincia_fuente',
+        alias: 'Provincia',
+        label: 'Escribe el nombre de la Provincia',
+      }
+    ],
+    infoFields: [
+      {
+        field: 'nombre',
+        alias: 'Nombre '
+      },
+      {
+        field: 'imagen',
+        alias: 'imagen'
+      },
+      {
+        field: 'tipo',
+        alias: 'Tipo'
+      },
+      {
+        field: 'enlace',
+        alias: 'Ficha'
+      },
+      {
+        field: 'municipio_fuente',
+        alias: 'Municipio'
+      },
+      {
+        field: 'provincia_fuente',
+        alias: 'Provincia'
+      }
+    ]
+  }
 }
 
 
 
 const configDirectorioEmpresas = {
-  options:{
+  options: {
     position: 'TL'
   },
   config: {
-  title: 'Buscador de Espacios Productivos',
-  geosearchUrl: 'https://www.juntadeandalucia.es/institutodeestadisticaycartografia/geobusquedas/eepp-f1_directorio/search?',
-  maxResults: 100,
-  fields: [
-    {
-      field: 'razon_soci',
-      alias: 'Nombre',
-      label: 'Escribe el nombre (Razón social)',
-    }
-  ],
-  infoFields: [
-    {
-      field: 'razon_soci',
-      alias: 'Nombre '
-    },
-    {
-      field: 'actividad',
-      alias: 'Actividad'
-    },
-    {
-      field: 'estrato',
-      alias: 'asalariados'
-    },
-    {
-      field: 'nombre',
-      alias: 'Espacio Productivo'
-    },
-    {
-      field: 'direccion',
-      alias: 'Dirección'
-    }
-  ]
-}}
+    title: 'Buscador de Espacios Productivos',
+    geosearchUrl: 'https://www.juntadeandalucia.es/institutodeestadisticaycartografia/geobusquedas/eepp-f1_directorio/search?',
+    maxResults: 100,
+    fields: [
+      {
+        field: 'razon_soci',
+        alias: 'Nombre',
+        label: 'Escribe el nombre (Razón social)',
+      }
+    ],
+    infoFields: [
+      {
+        field: 'razon_soci',
+        alias: 'Nombre '
+      },
+      {
+        field: 'actividad',
+        alias: 'Actividad'
+      },
+      {
+        field: 'estrato',
+        alias: 'asalariados'
+      },
+      {
+        field: 'nombre',
+        alias: 'Espacio Productivo'
+      },
+      {
+        field: 'direccion',
+        alias: 'Dirección'
+      }
+    ]
+  }
+}
 
 
 
 const configEspaciosProductivos = {
-  options:{
+  options: {
     position: 'TL'
   },
   config: {
-  title: 'Buscador de Espacios Productivos',
-  geosearchUrl: 'https://www.juntadeandalucia.es/institutodeestadisticaycartografia/geobusquedas/eepp-f1/search?',
-  maxResults: 100,
-  fields: [
-    {
-      field: 'municipio',
-      alias: 'Municipio',
-      label: 'Escribe el nombre del Municipio',
-    },
-    {
-      field: 'provincia',
-      alias: 'Provincia',
-      label: 'Escribe el nombre de la Provincia',
-    },
-    {
-      field: 'nombre',
-      alias: 'Nombre',
-      label: 'Escribe el nombre del Espacio Productivo',
-    }
-  ],
-  infoFields: [
-    {
-      field: 'nombre',
-      alias: 'Nombre '
-    },
-    {
-      field: 'tipologia',
-      alias: 'Tipología'
-    },
-    {
-      field: 'municipio',
-      alias: 'Municipio'
-    },
-    {
-      field: 'provincia',
-      alias: 'Provincia'
-    }
-  ]
-}}
+    title: 'Buscador de Espacios Productivos',
+    geosearchUrl: 'https://www.juntadeandalucia.es/institutodeestadisticaycartografia/geobusquedas/eepp-f1/search?',
+    maxResults: 100,
+    fields: [
+      {
+        field: 'municipio',
+        alias: 'Municipio',
+        label: 'Escribe el nombre del Municipio',
+      },
+      {
+        field: 'provincia',
+        alias: 'Provincia',
+        label: 'Escribe el nombre de la Provincia',
+      },
+      {
+        field: 'nombre',
+        alias: 'Nombre',
+        label: 'Escribe el nombre del Espacio Productivo',
+      }
+    ],
+    infoFields: [
+      {
+        field: 'nombre',
+        alias: 'Nombre '
+      },
+      {
+        field: 'tipologia',
+        alias: 'Tipología'
+      },
+      {
+        field: 'municipio',
+        alias: 'Municipio'
+      },
+      {
+        field: 'provincia',
+        alias: 'Provincia'
+      }
+    ]
+  }
+}
 
 
 const configMunicipos = {
-  options:{
+  options: {
     position: 'TR'
   },
   config: {
-  title: 'Buscador de Municipios',
-  geosearchUrl: 'http://geobusquedas-sigc.juntadeandalucia.es/sigc/search?',
-  maxResults: 200,
-  fields: [
-    {
-      field: 'municipio',
-      alias: 'Municipio',
-      label: 'Escribe el nombre del Municipio',
-    }
-  ],
-  infoFields: [
-    {
-      field: 'municipio',
-      alias: 'Municipio'
-    },
-    {
-      field: 'nombre_provincia',
-      alias: 'Provincia'
-    }
-  ]
-}}
+    title: 'Buscador de Municipios',
+    geosearchUrl: 'http://geobusquedas-sigc.juntadeandalucia.es/sigc/search?',
+    maxResults: 200,
+    fields: [
+      {
+        field: 'municipio',
+        alias: 'Municipio',
+        label: 'Escribe el nombre del Municipio',
+      }
+    ],
+    infoFields: [
+      {
+        field: 'municipio',
+        alias: 'Municipio'
+      },
+      {
+        field: 'nombre_provincia',
+        alias: 'Provincia'
+      }
+    ]
+  }
+}
 
 const configPlayas = {
-  options:{
+  options: {
     position: 'BL'
   },
   config: {
-  title: 'Buscador de Playas',
-  geosearchUrl: 'https://www.juntadeandalucia.es/institutodeestadisticaycartografia/geobusquedas/playas/search?',
-  maxResults: 100,
-  fields: [
-    {
-      field: 'nombre_playa',
-      alias: 'Playa',
-      label: 'Escribe el nombre de la Playa',
-    },
-    {
-      field: 'nombre_municipio',
-      alias: 'Municipio',
-      label: 'Escribe el nombre del Municipio',
-    },
-    {
-      field: 'provincia',
-      alias: 'Provincia',
-      label: 'Escribe el nombre de la Provincia'
-    }
-  ],
-  infoFields: [
-    {
-      field: 'nombre_playa',
-      alias: 'Playa'
-    },
-    {
-      field: 'nombre_municipio',
-      alias: 'Municipio'
-    },
-    {
-      field: 'aseos',
-      alias: 'Aseos'
-    },
-    {
-      field: 'salvamento_y_socorrismo',
-      alias: 'Salvamento y Socorrismo'
-    }
-  ]
-}}
+    title: 'Buscador de Playas',
+    geosearchUrl: 'https://www.juntadeandalucia.es/institutodeestadisticaycartografia/geobusquedas/playas/search?',
+    maxResults: 100,
+    fields: [
+      {
+        field: 'nombre_playa',
+        alias: 'Playa',
+        label: 'Escribe el nombre de la Playa',
+      },
+      {
+        field: 'nombre_municipio',
+        alias: 'Municipio',
+        label: 'Escribe el nombre del Municipio',
+      },
+      {
+        field: 'provincia',
+        alias: 'Provincia',
+        label: 'Escribe el nombre de la Provincia'
+      }
+    ],
+    infoFields: [
+      {
+        field: 'nombre_playa',
+        alias: 'Playa'
+      },
+      {
+        field: 'nombre_municipio',
+        alias: 'Municipio'
+      },
+      {
+        field: 'aseos',
+        alias: 'Aseos'
+      },
+      {
+        field: 'salvamento_y_socorrismo',
+        alias: 'Salvamento y Socorrismo'
+      }
+    ]
+  }
+}
 
 //const mp = new Searchpanel(configFuentesyManantiales);
 //const mp = new Searchpanel(configDirectorioEmpresas);
